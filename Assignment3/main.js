ada090de02c35e7dac590d76c32199b50485a7b6
nidapervez <nnpervez333@gmail.com>
2024-03-10 20:58:53 -0700

    first commit

---
+++ Assignment3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Storing the famous quote and ita author
+let quote = "Nothing is impossile in this world";
+let author = "Nida Pervez";
+//Printing the message
+console.log(`"${quote}"-${author}`);
---
+++ Assignment3/main.ts
@@ -0,0 +1,5 @@
+//Storing the famous quote and ita author
+let quote:string= "Nothing is impossile in this world"
+let author:string= "Nida Pervez"
+//Printing the message
+console.log(`"${quote}"-${author}`);
