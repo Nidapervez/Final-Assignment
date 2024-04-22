3cd3d528c1e267ab673b588cf880fa35abf949ff
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 02:11:06 -0700

    Update main.ts

--- Assignment22/main.ts
+++ Assignment22/main.ts
@@ -1,3 +1,8 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen.
+//Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+
+
 //creating  an array
 let errorarray:number[] =[21,22,23,24];
 //creating an intentional error
