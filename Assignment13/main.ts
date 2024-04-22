4e87fa933c0cb5dcc3930ab2d7f90bfc54307ca7
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 01:56:59 -0700

    Update main.ts

--- Assignment13/main.ts
+++ Assignment13/main.ts
@@ -1,3 +1,6 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
+//Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
 //favorite mode of transportation
 const favoritetransportation:string[]=["motorcycle", "car", "bicycle", "scooter", "electric skateboard"]
 //Print each transportation's name by accessing each element with series of statements
