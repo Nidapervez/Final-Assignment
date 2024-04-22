3931e4c9581abcf7c7ca1ff0ba8c4c043042cbb9
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 02:05:50 -0700

    Update main.ts

--- Assignment19/main.ts
+++ Assignment19/main.ts
@@ -1,3 +1,6 @@
+//Dinner Guests:
+//Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
 let guestlist= ["Nida", "Hira", "Anum", "Umama"];
 //guestlist.forEach(guest => console.log(`Hi! ${guest}, would you like to have dinner with me?`));
 let lenghtGuest:number= guestlist.length;
