248f49dafdaff8557b4a286fe974e847e0aa4460
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 02:29:28 -0700

    Update main.ts

--- Assignment32/main.ts
+++ Assignment32/main.ts
@@ -1,3 +1,10 @@
+
+//Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+//• Make a list of five or more usernames called current_users.
+
+//• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+
+
 //List of current users
 let current_users = ["john", "emma", "alex", "sophia", "mike"];
 // List of new users
