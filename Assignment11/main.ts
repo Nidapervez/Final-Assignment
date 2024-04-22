c294b75324b7c59e58f1525f3a52760408f3676c
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 01:55:01 -0700

    Update main.ts

--- Assignment11/main.ts
+++ Assignment11/main.ts
@@ -1,3 +1,6 @@
+//Names: Store the names of a few of your friends in a array called names.
+//Print each person’s name by accessing each element in the list, one at a time.
+
 // Define an array of names
 const names: string[] = ["Alice", "Bob", "Charlie", "David", "Eve"];
 
