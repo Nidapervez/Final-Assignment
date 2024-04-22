1f9dd8b336da29be96349ae277250ee1c9bd210b
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 01:39:00 -0700

    Update main.ts

--- Assignment5/main.ts
+++ Assignment5/main.ts
@@ -1,3 +1,6 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+//Then compose your message and store it in a new variable called message. Print your message.
+
 //Storing famous person name in a variable
 let famous_person:string = "Salman"
 //storing a name in a variable
