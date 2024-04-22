4c161d7296cd5773998602a656ffdf376960cd4d
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 01:58:30 -0700

    Update main.ts

--- Assignment15/main.ts
+++ Assignment15/main.ts
@@ -1,3 +1,6 @@
+//Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. 
+//You’ll have to think of someone else to invite.
+
 let guestlist: any[] = ["Nida", "Hira", "Anum", "Umama"];
 let notComing: any[]=guestlist[1];
 console.log(notComing,"is not coming");
