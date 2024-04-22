45f72d22ba3b076e739b007adbf6f86470b839a9
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 02:10:01 -0700

    Update main.ts

--- Assignment21/main.ts
+++ Assignment21/main.ts
@@ -1,3 +1,7 @@
+
+//They think of something you could store in a TypeScript Object.
+//Write a program that creates Objects containing these items.
+
 interface itcourse {
     coursename:string;
     location:string;
