b350ff6373ccad94b4d06f2a4ba912fa99b8af0a
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 01:19:11 -0700

    Update main.ts

--- Assignment1/main.ts
+++ Assignment1/main.ts
@@ -1,4 +1,6 @@
-//Assignment # 1
+//Assignment # 1Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+
 //Storing a person name in a variale
 
 let personName : string = "Nida"
