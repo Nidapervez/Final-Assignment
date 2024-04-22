73758eb083acce10d07b8ec7d451979bd440a2cc
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 02:34:23 -0700

    Update main.ts

--- Assignment36/main.ts
+++ Assignment36/main.ts
@@ -1,4 +1,10 @@
 
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+//The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+
+
+
 function make_shirt(size:string,text:string){
     return console.log(`Here is your ${size} shirt,with this message ${text}`)}
     var result= make_shirt("large","Its Amazing")
