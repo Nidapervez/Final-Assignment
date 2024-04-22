23d1e3296bbc06b86012f4597c99b8f2c60c1361
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 01:22:21 -0700

    Update main.ts

--- Assignment3/main.ts
+++ Assignment3/main.ts
@@ -1,3 +1,7 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+//Your output should look something like the following, including the quotation marks:
+
+
 //Storing the famous quote and ita author
 let quote:string= "Nothing is impossile in this world"
 let author:string= "Nida Pervez"
