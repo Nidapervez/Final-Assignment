4540e829885a5789db77be401dd8a5bd59e4b523
Nida Pervez <159867017+Nidapervez@users.noreply.github.com>
2024-04-22 01:53:18 -0700

    Update main.js

--- Assignment9/main.js
+++ Assignment9/main.js
@@ -1,4 +1,5 @@
-"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. 
+//Print that message.
 //Storing favorite number in a variable.
 let favoriteNumber = 10;
 //Creating a message that reveals my favorite number.
